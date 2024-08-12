bff4bc42e9d19b7a265ee2154c8e07c60a70873f
Iman Salehi <cse.isalehi@gmail.com>
2024-08-12 23:28:46 +0330

    using QueryParam with @RequestParam

--- src/main/java/be/jimsa/springtutorial/mvc/controllers/CourseController.java
+++ src/main/java/be/jimsa/springtutorial/mvc/controllers/CourseController.java
@@ -1,11 +1,19 @@
 package be.jimsa.springtutorial.mvc.controllers;
 
+import be.jimsa.springtutorial.mvc.models.responses.UserResponse;
 import be.jimsa.springtutorial.mvc.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 @Controller
 @Slf4j
@@ -18,9 +26,16 @@ public class CourseController {
     }
 
     @RequestMapping("/course")
-    public String courseListDisplay(Model model) {
+    public String courseDisplay(@RequestParam(required = false) Optional<String> type, Model model) {
 
-        model.addAttribute("data", userService.getAllData());
+        Map<String, Supplier<List<UserResponse>>> dataMap = new HashMap<>();
+        dataMap.put("math", userService::getMathData);
+        dataMap.put("english", userService::getEnglishData);
+
+        model.addAttribute("data", type
+                .map(str -> dataMap.getOrDefault(str.toLowerCase(), userService::getAllData).get())
+                .orElseGet(userService::getAllData)
+        );
 
         return "course.html";
     }
@@ -32,13 +47,12 @@ public class CourseController {
             model.addAttribute("data", userService.getMathData());
         } else if (display.equalsIgnoreCase("english")) {
             model.addAttribute("data", userService.getEnglishData());
-        }else {
+        } else {
             model.addAttribute("data", userService.getAllData());
         }
 
         return "course.html";
     }
-
 
 
 }
